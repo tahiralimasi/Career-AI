248a2ff1331b04757bd05be90ec4c5e7c823cb99
tahiralimasi <tahiralimasi101@gmail.com>
2026-09-07 13:05:53 +0530

    change

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -36,7 +36,7 @@ export const generateAIInsights = async (industry) => {
         content: prompt,
       },
     ],
-    model: "llama-3.3-70b-versatile",
+    model: "openai/gpt-oss-120b",
     temperature: 0.5,
     max_tokens: 2048,
   });
