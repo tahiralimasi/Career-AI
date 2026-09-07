035097e5275d1f66694cadafb30bc6710249cb11
tahiralimasi <tahiralimasi101@gmail.com>
2026-09-07 13:08:12 +0530

    chnage

--- actions/resume.js
+++ actions/resume.js
@@ -99,7 +99,7 @@ export async function improveWithAI({ current, type }) {
           content: prompt,
         },
       ],
-      model: "llama-3.3-70b-versatile",
+      model: "openai/gpt-oss-120b",
       temperature: 0.7,
       max_tokens: 500,
     });
